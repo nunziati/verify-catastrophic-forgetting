2d0c83f647a6d720ccfebe433b7cacb4ebe501e7
nunziati <giacomo.nunziati.0@gmail.com>
2022-01-31 17:25:59 +0100

    everything about numpy, sobsituted with torch

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import torchvision
 import torchvision.transforms as transforms
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-import numpy as np
+import nets
 
 def interactive():
     while(True):
         a = input(">>> ")
         if a=="q": break
-        eval(a)
+        exec(a)
 
 def show_example(a):
     plt.imshow(a[0].squeeze())
@@ -18,41 +18,44 @@ def show_example(a):
 class DatasetMerger(torch.utils.data.Dataset):
     def __init__(self, datasets_list, mode="torch"):
         self.datasets_list = datasets_list
-        indexes_list = []
+        data_indexes_list = []
         self.class_map = {}
         for dataset_index, dataset in enumerate(self.datasets_list):
-            dim = len(dataset)
-            dataset_index_array = np.full(dim, dataset_index, dtype=np.int64).reshape((1, dim))
-            example_index_array = np.arange(dim, dtype=np.int64).reshape((1, dim))
-            array = np.concatenate((dataset_index_array, example_index_array))
-            indexes_list.append(array)
-            self.class_map.update({(dataset_index, x): x + len(self.class_map) for x in np.sort(np.unique(dataset.targets))})
-        self.data_indexes = np.concatenate(indexes_list, axis=1).transpose()
+            n_samples = len(dataset)
+            dataset_index_array = torch.full((1, n_samples), dataset_index, dtype=torch.int64)
+            example_index_array = torch.arange(n_samples, dtype=torch.int64).reshape((1, -1))
+            data_index_array = torch.cat((dataset_index_array, example_index_array))
+            data_indexes_list.append(data_index_array)
+            self.class_map.update({(dataset_index, x.item()): x.item() + len(self.class_map) for x in torch.unique(torch.tensor(dataset.targets), sorted=True)})
+        self.data_indexes = torch.cat(data_indexes_list, dim=1).transpose(0, 1)
         self.reverse_class_map = {self.class_map[x]: x for x in self.class_map}
-        
-            
 
     def __getitem__(self, index):
         if isinstance(index, int):
             index = self.data_indexes[index]
-        return index, self.datasets_list[index[0]][index[1]]
+        dataset_index = index[0].item()
+        example_index = index[1]
+        data, label = self.datasets_list[dataset_index][example_index]
+        label = self.class_map[(dataset_index, label)]
+        return index, (data, label)
 
     def shuffle(self):
-        self.data_list = np.random.shuffle(self.data_indexes)
+        indexes = torch.randperm(self.data_indexes.shape[0])
+        self.data_indexes = self.data_indexes[indexes]
 
     def dataset_wise_sort_by_label(self):
         # maybe it exists a better way of doing this? maybe without the for loops
-        self.data_indexes = self.data_indexes[np.argsort(self.data_indexes[:, 0]), :]
+        self.data_indexes = self.data_indexes[torch.argsort(self.data_indexes[:, 0]), :]
 
         data_indexes_list = []
 
         for dataset_index, dataset in enumerate(self.datasets_list):
-            dim = len(dataset)
-            dataset_index_array = np.full(dim, dataset_index, dtype=np.int64).reshape((1, dim))
-            example_index_array = np.argsort(dataset.targets).reshape((1, dim))
-            array = np.concatenate((dataset_index_array, example_index_array))
-            data_indexes_list.append(array)
-        self.data_indexes = np.concatenate(data_indexes_list, axis=1).transpose()
+            length = len(dataset)
+            dataset_index_array = torch.full((1, length), dataset_index, dtype=torch.int64)
+            example_index_array = torch.argsort(torch.tensor(dataset.targets)).reshape((1, length))
+            data_index_array = torch.cat((dataset_index_array, example_index_array))
+            data_indexes_list.append(data_index_array)
+        self.data_indexes = torch.cat(data_indexes_list, axis=1).transpose(0, 1)
 
 class ShallowMLP(torch.nn.Module):
     def __init__(self):
@@ -94,29 +97,19 @@ train_set_mnist = torchvision.datasets.MNIST(root='./data', train=True, download
 test_set_mnist = torchvision.datasets.MNIST(root='./data', train=False, download=True, transform=transform_mnist)
 print("Datasets correctly downloaded")
 
-print("Loading data...")
 """train_loader_cifar10 = torch.utils.data.DataLoader(train_set_cifar10, batch_size=1, shuffle=False, num_workers=2)
 test_loader_cifar10 = torch.utils.data.DataLoader(test_set_cifar10, batch_size=4, shuffle=False, num_workers=2)
 train_loader_mnist = torch.utils.data.DataLoader(train_set_mnist, batch_size=1, shuffle=False, num_workers=2)
 test_loader_mnist = torch.utils.data.DataLoader(test_set_mnist, batch_size=4, shuffle=False, num_workers=2)
 """
-print("Dataset correctly loaded!")
-
-train_set = DatasetMerger((train_set_cifar10, train_set_mnist))
-
-train_set.shuffle()
-
-train_set.dataset_wise_sort_by_label()
-
-for index, data in train_set:
-    print(index, "\t", data[1])
 
 print("Sorting and merging datasets...")
+train_set = DatasetMerger((train_set_cifar10, train_set_mnist))
+test_set = DatasetMerger((test_set_cifar10, test_set_mnist))
+train_set.dataset_wise_sort_by_label()
 
-# TO DO: sort_dataset(cifar10_train)
-# TO DO: sort_dataset(mnist_train)
-# TO DO: merge_dataset(cifar10_train, mnist_train)
-# TO DO: merge_dataset(cifar10_test, mnist_test) # no need for sorting them
+train_set_loader = torch.utils.data.DataLoader(train_set, batch_size=1, shuffle=False, num_workers=2)
+test_set_loader = torch.utils.data.DataLoader(test_set, batch_size=64, shuffle=False, num_workers=2)
 print("Training set correctly sorted!")
 
 print("Creating models...")
